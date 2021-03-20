952f04af8370333ef175d1e6dae29cf95ace29ef
Jerome Gero <JeromeRGero@gmail.com>
2021-03-20 13:53:24 -0400

    - Adding .env change to house private strings for MongoDB connection. -  Adding decontruction of a user's vote to save in the MongoDB

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import discord
+import datetime
 import os
+import pymongo
+from pymongo import MongoClient
 from decouple import config
 
+
 client = discord.Client()
+cluster = MongoClient(config('MONGO_CONNECTION_URL'))
+db = cluster[config('MONGO_DATABASE_NAME')]
+collection = db[config('MONGO_USERVOTES_COLLECTION')]
+
 
 @client.event
 async def on_ready():
@@ -14,22 +22,50 @@ async def on_message(message):
     if message.author.id == client.user.id:
         return
 
-    print('The author is {0} with id {1}'.format(message.author.name, message.author.id))
-
-    print("The channel is {0}".format(message.channel.name))
-
     if message.channel.name == 'votes':
         await message.add_reaction("\U0001F44D")
         await message.add_reaction("\U0001F44E")
         await message.add_reaction("\u2753")
-
-    if message.content.startswith('$demprime'):
+        msgDatetime = datetime.datetime.now()
+        msgExperationDatetime = msgDatetime + datetime.timedelta(days=3)
+        post = {
+            'messageId': message.id,
+            "messageContent": message.content,
+            "authorId": message.author.id,
+            "authorName": message.author.name,
+            "voteCreatedAt": {
+                'year': msgDatetime.year,
+                'month': msgDatetime.month,
+                'day': msgDatetime.day,
+                'minute': msgDatetime.minute,
+            },
+            "voteExpireAt": {
+                'year': msgExperationDatetime.year,
+                'month': msgExperationDatetime.month,
+                'day': msgExperationDatetime.day,
+                'minute': msgExperationDatetime.minute,
+            },
+            "rectionsCount": {
+                'thumbsup': 0,
+                'thumbsdown': 0,
+                'question': 0,
+            }
+        }
+        print(post)
+        # collection.insert_one(post)
+    elif message.content.startswith('$demprime'):
         if message.content == '$demprime':
-            await message.channel.send('If you require assistance, type `$demprime help`')
+            await message.channel.send('If you require assistance, type `$demprime help`.')
         elif message.content == '$demprime help':
             await message.channel.send(
-                'Democracy Prime is online.\nAll commands can be executed with `$demprime` \{argument_here\}')
-
+                'Democracy Prime is online.\n' +
+                'All commands can be executed with `$demprime \{argument_here\}` ' +
+                'without the curly brackets.'
+            )  # if more params are needed I will print out a list of themin this send block
+        elif message.content.startswith('$demprime check'):
+            msg = str(message.content).split(' ')
+            if (len(msg) >= 3):
+                username = msg[2]
+                print(username)
 
 client.run(config('TOKEN'))
-
